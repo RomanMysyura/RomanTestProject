7712fdbe8730b38f9828195a5f394af8a468acd7
Roman Mysyura <rmysyura@cendrassos.net>
2024-04-10 18:56:44 +0200

    Initial

---
+++ deploy/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -f app.tgz
+mkdir -p -m 775 ./storage/app/public ./storage/framework/cache ./storage/framework/sessions ./storage/framework/testing ./storage/framework/views ./storage/logs  
+(chgrp -R www-data storage ; true)
+(chmod -R g+rwX storage ; true)
+npm update
+composer update
